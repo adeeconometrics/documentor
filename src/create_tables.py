0864032ff2e466825b500a2aa6a7e1c2e4070eb0
Dave Amiana <amianism25@gmail.com>
2024-08-08 19:41:07 +0800

    savepoint factory method for autogen enum

--- src/create_tables.py
+++ src/create_tables.py
@@ -1,51 +1,18 @@
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Tuple
 from enum import Enum, auto
 
 from pydantic import BaseModel, field_validator
 from tabulate import tabulate
 
-class TableFmt(Enum):
-    """Represents the format of the table."""
-    
-    def _generate_next_value_(name, start, count, last_values) -> str:
-        return name
-    
-    plain = auto()
-    simple = auto()
-    github = auto()
-    grid = auto()
-    simple_grid = auto()
-    rounded_grid = auto()
-    heavy_grid = auto()
-    mixed_grid = auto()
-    double_grid = auto()
-    fancy_grid = auto()
-    outline = auto()
-    simple_outline = auto()
-    rounded_outline = auto()
-    heavy_outline = auto()
-    mixed_outline = auto()
-    double_outline = auto()
-    fancy_outline = auto()
-    pipe = auto()
-    orgtbl = auto()
-    asciidoc = auto()
-    jira = auto()
-    presto = auto()
-    pretty = auto()
-    psql = auto()
-    rst = auto()
-    mediawiki = auto()
-    moinmoin = auto()
-    youtrack = auto()
-    html = auto()
-    unsafehtml = auto()
-    latex = auto()
-    latex_raw = auto()
-    latex_booktabs = auto()
-    latex_longtable = auto()
-    textile = auto()
-    tsv = auto()
+
+def enum_factory(enum_name: str, names: Tuple[str, ...]) -> Enum:
+    """Create an Enum class with the given name and names."""
+    TEnumClass = Enum(enum_name, {name: name for name in names})
+    return TEnumClass
+
+
+TableFmt: Enum = enum_factory('TableFmt', ('plain', 'simple', 'github', 'grid', 'fancy_grid', 'pipe', 'orgtbl', 'jira', 'presto', 'pretty',
+                        'psql', 'rst', 'mediawiki', 'moinmoin', 'youtrack', 'html', 'unsafehtml', 'latex', 'latex_raw', 'latex_booktabs', 'textile'))
 
 
 class TableData(BaseModel):
@@ -57,12 +24,15 @@ class TableData(BaseModel):
         """Validate that all values in the mapping have the same length."""
         for key, values in mapping.items():
             if len(values) != len(mapping[next(iter(mapping))]):
-                raise ValueError(f"Length of {key} does not match other columns")
+                raise ValueError(
+                    f"Length of {key} does not match other columns")
         return mapping
 
-def make_table(table:TableData, tbl_format:TableFmt = TableFmt.github) -> str:
+
+def make_table(table: TableData, tbl_format: Enum = TableFmt.mediawiki) -> str:
     """Convert the table to a table string."""
     return tabulate(table.mapping, headers="keys", tablefmt=tbl_format.value)
+
 
 if __name__ == '__main__':
     data = TableData(mapping={
