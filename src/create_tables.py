d332ae4e3299bbfdff78a6d0e6b50d4ccf8330c1
Dave Amiana <amianism25@gmail.com>
2024-08-08 20:17:50 +0800

    savepoint for generating payload

--- src/create_tables.py
+++ src/create_tables.py
@@ -1,5 +1,6 @@
-from typing import Dict, List, Any, Tuple
+from typing import Dict, List, Any, Tuple, Union
 from enum import Enum
+from pathlib import Path
 
 from pydantic import BaseModel, field_validator
 from tabulate import tabulate
@@ -36,13 +37,30 @@ def make_table(table: TableData, tbl_format: Enum = TableFmt.github) -> str:
     """Convert the table to a table string."""
     return tabulate(table.mapping, headers="keys", tablefmt=tbl_format.value)
 
-def generate_data(fields:Tuple[str, ...], t_size:int) -> TableData:
-    """Generate a table from the given data."""
-    return {
-        field: [fake.name() for _ in range(t_size)]
-        for field in fields
-    }
+def save_tbl_to_file(table: TableData, file_path: Union[Path,str], tbl_format: Enum = TableFmt.github) -> None:
+    """Save the table to a file."""
+    with open(file_path, mode='w', encoding='utf-8') as file:
+        file.write(make_table(table, tbl_format))
 
 if __name__ == '__main__':
-    data = TableData(mapping=generate_data(('name', 'address', 'email'), 5))
+
+    words_list:tuple[str,...] = (
+        "Compute", "Storage", "Network", "Virtual", "Machine", "Disk", "Blob", "Queue", "Table", "File",
+        "Container", "Resource", "Group", "KeyVault", "Identity", "Function", "Logic", "App", "Service", "API",
+        "Gateway", "Endpoint", "Subnet", "Firewall", "LoadBalancer", "ScaleSet", "VNet", "Traffic", "Manager", "CDN",
+        "Instance", "Cluster", "Namespace", "Policy", "Role", "Binding", "Certificate", "Automation", "Runbook", "Hybrid",
+        "Connector", "Event", "Hub", "Stream", "Analytics", "Data", "Lake", "Factory", "Synapse", "Pipeline",
+        "Trigger", "Monitor", "Alert", "Log", "Insights", "Metric", "Dashboard", "Workspace", "Backup", "Recovery",
+        "Site", "Advisor", "Cost", "Management", "Billing", "Compliance", "Security", "Center", "AzureAD", "Bastion",
+        "DevOps", "Pipeline", "Artifact", "Release", "Repository", "Board", "Wiki", "Test", "Agent", "Pool",
+        "Environment", "Secret", "Access", "Control", "Route", "Table", "Uptime", "SLA", "Latency", "Throughput",
+        "Cache", "Redis", "SQL", "MySQL", "PostgreSQL", "CosmosDB", "MongoDB", "Backup", "Archive", "Tier"
+    )
+    data = TableData(mapping={
+        'parameter': ['_'.join( word.lower() for word in 
+                               fake.words(nb=3, ext_word_list=words_list)) for _ 
+                               in range(20)],
+        'value': [fake.sentence() for _ in range(20)],
+    })
+    save_tbl_to_file(data, Path('./data/data.md'), TableFmt.github)
     print(make_table(data))
